7ba3917f7a481eb4d0065e92df888c7211e73491
UnityExample <65411285+UnityExample@users.noreply.github.com>
2020-05-18 14:46:42 -0400

    Initial Commit

---
+++ Editor/CreateParentForSelected.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.EditorTools
+{
+    public class CreateParentForSelected
+    {
+        [MenuItem("Edit/Create Parent for Selected %g")]
+        public static void CreateParent() {
+
+            //get all selected transforms
+            var transforms = new List<Transform>(UnityEditor.Selection.transforms);
+
+            //nothing selected, don't do anything
+            if (transforms.Count <= 0) return;
+
+            //create a new zeroed gameObject
+            var parentObject = new GameObject("New Parent");
+            Undo.RegisterCreatedObjectUndo(parentObject, "Create Parent for Selected");
+
+            transforms.Sort((a, b) => { return a.GetSiblingIndex().CompareTo(b.GetSiblingIndex()); });
+
+            //if there is a shared parent, create the parent underneath it, and not in the root
+            var topMostParent = transforms[0].parent;
+
+            //search for the shared parent
+            foreach(var t in transforms) { 
+                if (t.parent != topMostParent) topMostParent = null;
+	        }
+
+            //move our new parent to the shared parent
+            parentObject.transform.SetParent(topMostParent, false);
+
+            //move our children to our new parent
+            foreach(var t in transforms) {
+                Undo.SetTransformParent(t, parentObject.transform, "Create Parent for Selected");
+	        }
+
+            UnityEditor.Selection.activeGameObject = parentObject;
+	    }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Create Parent for Selected
+
+## Description
+This tool adds an option to the Unity Editor under Edit > Create Parent for Selected. It creates an empty parent for the selected objects in the hierarchy.
+
+## Setup
+This tool is distributed as a UPM (Unity Package Manager) Package and works with no dependencies. You can add this tool to your project with its git URL through the Package Manager if you have git installed on your machine.
